cd232d51525860fe3be4e173aa5636ba04113257
NikosDourasEdu <146086873+NikosDourasEdu@users.noreply.github.com>
2023-09-26 14:24:29 +0300

    Update post.py

--- app/routers/post.py
+++ app/routers/post.py
@@ -11,7 +11,11 @@ router = APIRouter(
     tags=['Posts']
 )
 
+@router.get("/test")
+def test():
+    return {"message": "hellow from olympus"}
 
+    
 @router.get("/", response_model=List[schemas.PostOut])
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user),
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
